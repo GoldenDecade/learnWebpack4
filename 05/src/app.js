4f997995225d83411edc1040eee705c5e9b0dcf6
wangqiang <wangqiangb@litsoft.com.cn>
2019-03-17 21:21:52 +0800

    05 css: 总结 4种功能
    Signed-off-by: wangqiang <wangqiangb@litsoft.com.cn>

--- 05/src/app.js
+++ 05/src/app.js
@@ -1,3 +1,10 @@
+/*
+* target:
+* 1. 打包为link标签引入css文件
+* 2. 打包为style标签引入css样式
+* 3. 加载和卸载css文件
+* 4. 通过css.transform.js 文件 动态更改css样式
+* */
 import base from './assets/css/base.css'
 /*//动态 加载和卸载css 样式；
 let num = 0;
