a1d6b5ef60f8f78113af7e02999a35a227c98fee
wangqiang <wangqiangb@litsoft.com.cn>
2019-03-17 20:58:28 +0800

    05 css： （style） 1. 使用css-loader style-loader将css通过js动态插入到入口js文件中 2. style标签相对于link标签的优点
    Signed-off-by: wangqiang <wangqiangb@litsoft.com.cn>

--- 05/webpack.config.js
+++ 05/webpack.config.js
@@ -13,13 +13,26 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: [
+        /*use: [
           {
             loader: 'style-loader/url'
           },
           //  将css编译为文件才能通过link标签插入，所以需要file-loader；
           {
             loader: 'file-loader'
+          }
+        ]*/
+        //将css放在style标签中可以减少网络请求次数，提高响应时间；
+        //注意： 老式IE浏览器，对style标签的数量是有限制的
+        use: [
+          {
+            loader: 'style-loader',
+            options: {
+              singleton: true //将css文件处理为单个 style 标签 引入
+            }
+          },
+          {
+            loader: 'css-loader',
           }
         ]
       }
