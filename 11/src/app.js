69dae20195deeafb931fbfcf2bb671dc59e01a29
wangqiang <wangqiangb@litsoft.com.cn>
2019-03-18 22:44:08 +0800

    11: 使用本地文件来打包 1. resolve的alias 作用是 缩短文件目录名字 2. webpack.ProvidePlugin 作用是 提供全局变量名字
    Signed-off-by: wangqiang <wangqiangb@litsoft.com.cn>

--- 11/src/app.js
+++ 11/src/app.js
@@ -1,1 +1,12 @@
-console.log($('#app'));
+/*
+ * 第三方JS库
+ * 1. CDN: '<script></script>'标签引入即可
+ * 2. npm包管理: ProvidePlugin, 键是变量名, 值是第三方库名称
+ * 3. 本地Js文件: alias别名, 将本地库解析到对应的文件中. 例如示例中的 'jQuery'被解析到'src/vendor/jquery.min.js'
+*/
+// console.log($('#app'));
+// console.log(jQuery);
+// $('#app').addClass('new');
+jQuery('div').addClass('old')
+console.log(jQuery);
+console.log(jQuery('#box'));
--- 11/webpack.config.js
+++ 11/webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path')
 const webpack = require('webpack')
-
 module.exports = {
   entry: {
     app: './src/app.js'
@@ -13,12 +12,15 @@ module.exports = {
   },
   resolve: {
     alias: {
-      // $: 'jquery/dist/jquery.min.js',
+      //配置别名，在项目中缩减引用路径
+      // jQuery$: path.resolve(__dirname, 'src/vendor/jquery.min.js'),
     }
   },
   plugins: [
+    //  提供全局变量的，在模块中使用的时候就不需要require了
     new webpack.ProvidePlugin({
-      // $: 'jquery',
+      // $: 'jquery',// 引用的是 npm 包
+      jQuery: 'jQuery',// 指向的是本地src/vendor/jquery.min.js
     })
   ]
 }
