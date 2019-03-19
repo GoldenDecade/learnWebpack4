c088eeb333e1189d31bd229fdfd81448b77ba9c2
wangqiang <wangqiangb@litsoft.com.cn>
2019-03-19 11:34:55 +0800

    path 和 publicPath； 修改publicPath，使用相对路径

--- 13/webpack.config.js
+++ 13/webpack.config.js
@@ -9,7 +9,8 @@ module.exports = {
     app: './src/app.js'
   },
   output: {
-    publicPath: __dirname + "/dist/", // js引用路径或者CDN地址
+    // publicPath: __dirname + "/dist/", // js引用路径或者CDN地址
+    publicPath: "./", // js引用路径或者CDN地址
     path: path.resolve(__dirname, "dist"), // 打包文件的输出目录
     filename: "[name]-[hash:5].bundle.js",
     chunkFilename: "[name]-[hash:5].chunk.js"
