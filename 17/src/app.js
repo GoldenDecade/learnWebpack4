24b651a74000c6132b28c755323e1931bd2aa44e
wangqiang8 <wangqiang8@kingsoft.com>
2020-04-29 09:15:51 +0800

    feat: sdf

--- 17/src/app.js
+++ 17/src/app.js
@@ -4,8 +4,11 @@ import './assets/css/index.css';
 
 import './assets/fonts/iconfont.css';
 
-let files = require.context('./assets/js', false, /\.js$/);let v = 123;
+let files = require.context('./assets/js', false, /\.js$/) ;
 /* eslint-disable */
 console.log(files.keys());
 
 /* eslint-enable */
+function a() {
+    return a;
+}
